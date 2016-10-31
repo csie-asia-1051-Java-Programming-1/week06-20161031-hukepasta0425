085fa8c35987126be47a515468a7c0b402c666ea
hukepasta0425 <105021031@live.asia.edu.tw>
2016-10-31 16:01:32 +0800

    10/31
    GOGO

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,32 @@
 package hw;
 
+import java.util.*;
+
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner src = new Scanner(System.in);
+		System.out.println("請輸入溫度: ");
+		float t = src.nextInt();
+		System.out.println("F->C請輸入1 ");
+		System.out.println("C->F請輸入2 ");
+		float a = src.nextInt();
+		float b = fun1(t, a);
+		System.out.println(b);
 
+	}
+
+	public static float fun1(float t1, float a1) {
+		float sum = 0;
+
+		if (a1 == 1) {
+			sum = (t1 - 32) * 5 / 9;
+		} else {
+			sum = t1 * 9 / 5 + 32;
+		}
+
+		return sum;
 	}
 
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,31 @@
 package hw;
 
+import java.util.*;
+
 public class hw02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner src = new Scanner(System.in);
+		System.out.println("請輸入性別(男1/女2): ");
+		int val= src.nextInt();
+		System.out.println("請輸入身高(CM): ");
+		int h=src.nextInt();
+		fun1(val,h);
+		}
+		
+		
+		public static void fun1 (int val1, int h1) {
+			float sum = 0;
 
+			if (val1 ==1) {
+				sum = (h1-170)*0.6f+62;
+			} else {
+				sum =(h1-158)*0.5f+52;
+			}
+			System.out.println(sum);
+			
+		}
 	}
 
-}
+
